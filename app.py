bf4ff0596a306483a29ce513ede1342256f258c5
vivek.panchal <vivek.panchal@veritawall.com>
2024-02-02 00:05:36 +0530

    deployment essentials

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def get_fonts():
     fonts = get_available_fonts()
     return jsonify({'fonts': fonts})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
